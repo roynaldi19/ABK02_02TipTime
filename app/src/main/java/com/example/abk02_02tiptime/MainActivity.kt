adedb724f775c0405426d0dcfcf1d60f0b6e4eed
roynaldi19 <roynaldi19@gmail.com>
2021-06-23 20:30:39 +0700

    comit 1

--- app/src/main/java/com/example/abk02_02tiptime/MainActivity.kt
+++ app/src/main/java/com/example/abk02_02tiptime/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.abk02_02tiptime
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.abk02_02tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        activityMainBinding.calculateButton.setOnClickListener {
+            calculateTip()
+        }
     }
+
+    fun calculateTip() {
+        val cost = activityMainBinding.costOfService.text.toString().toDouble()
+
+        val selectedId = activityMainBinding.tipOptions.checkedRadioButtonId
+        val tipPercentage = when (selectedId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+
+        var tip = tipPercentage * cost
+
+        val roundUp = activityMainBinding.roundUpSwitch.isChecked
+        if(roundUp) {
+            tip = kotlin.math.ceil(tip)
+        }
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        activityMainBinding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
 }
